fd36aedc0ea93668e3362196ecc2560b42816ae9
vanessasoteroacc <v.sotero.barros@accenture.com>
2024-07-06 21:36:44 -0300

    Refector: Pequenas melhorias

--- cypress/allRunners.js
+++ cypress/allRunners.js
@@ -2,3 +2,5 @@ import "./e2e/1_cadastro_usuario.cy";
 import "./e2e/2_login.cy";
 import "./e2e/3_add_produto.cy";
 import "./e2e/logout.cy";
+import "./e2e/busca_produto.cy";
+import "./e2e/detalhe_pedido.cy";
